fffc7a9ac7f30c80fc3a3a5d7ba7ef0cca23f367
markusremplbauer <markus.remplbauer@gmx.at>
2022-10-24 12:37:45 +0200

    update template component

--- frontend-beeyond/app/src/app/modules/template/pages/template-create/template-create.component.ts
+++ frontend-beeyond/app/src/app/modules/template/pages/template-create/template-create.component.ts
@@ -3,6 +3,7 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BackendApiService } from '../../../../core/services/backend-api.service';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ThemeService } from '../../../../core/services/theme.service';
 
 @Component({
   selector: 'app-template-create',
@@ -13,17 +14,30 @@ export class TemplateCreateComponent implements OnInit {
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
   thirdFormGroup: FormGroup;
-
-  monacoOptions = { language: 'yaml', scrollBeyondLastLine: false };
-
   wildcards: string[] = [];
+
+  monacoOptions = {
+    language: 'yaml',
+    scrollBeyondLastLine: false,
+    theme: this.themeService.isDarkTheme.value ? 'vs-dark' : 'vs-light',
+    automaticLayout: true
+  };
 
   constructor(
     private router: Router,
     private fb: FormBuilder,
     private backendApiService: BackendApiService,
-    private snackBar: MatSnackBar
-  ) {}
+    private snackBar: MatSnackBar,
+    private themeService: ThemeService
+  ) {
+    this.themeService.isDarkTheme.subscribe(value => {
+      this.monacoOptions = { ...this.monacoOptions, theme: value ? 'vs-dark' : 'vs-light' };
+    });
+  }
+
+  get fields(): FormArray {
+    return this.secondFormGroup.controls.fields as FormArray;
+  }
 
   ngOnInit(): void {
     this.firstFormGroup = this.fb.group({
@@ -43,12 +57,12 @@ export class TemplateCreateComponent implements OnInit {
       this.wildcards = [];
       this.fields.clear();
 
-      const regex = /%([\w]+)%/g;
+      const regex = /%([\w-]+)%/g;
       let match;
 
       do {
         match = regex.exec(content);
-        if (match) {
+        if (match && !this.wildcards.includes(match[1])) {
           this.wildcards.push(match[1]);
           this.fields.push(this.createWildcardField(match[1]));
         }
@@ -66,7 +80,8 @@ export class TemplateCreateComponent implements OnInit {
       this.router.navigate(['template']).then(navigated => {
         if (navigated) {
           this.snackBar.open('Your Template was created', 'close', {
-            duration: undefined
+            duration: 2000,
+            panelClass: ['mat-drawer-container']
           });
         }
       });
@@ -79,9 +94,5 @@ export class TemplateCreateComponent implements OnInit {
       label: ['', [Validators.required, Validators.maxLength(255)]],
       description: ['', Validators.maxLength(255)]
     });
-  }
-
-  get fields(): FormArray {
-    return this.secondFormGroup.controls.fields as FormArray;
   }
 }
